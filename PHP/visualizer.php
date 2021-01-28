6087bbd408f0c5b342f30f308619844235392ce3
Federico Bertoncini <federico.bertoncini@gmail.com>
2021-01-28 11:50:23 +0100

    Update visualizer.php

--- PHP/visualizer.php
+++ PHP/visualizer.php
@@ -18,7 +18,7 @@ if (isset($_GET["button2"])) {
 
     if ($stmt = $mysqli->prepare($sql)) {
         // Bind variables to the prepared statement as parameters
-        /*$stmt->bind_param("i", $param_ID);
+        /*$stmt->bind_param("$i", $param_ID);
 
         // Set parameters
         $param_ID = 1;*/
@@ -38,6 +38,64 @@ if (isset($_GET["button2"])) {
     $ps = "";
 }
 
+function tableChecker($obj, $cond)
+{
+    $i = $y = 0;
+    $return = null;
+    if ($cond == "Q") {
+        foreach ($obj as $value) {
+            if ($value->livello == "Q") {
+                $return[$y] = $value;
+                $y++;
+            } else {
+                $i++;
+            }
+        }
+        return $return;
+    } elseif ($cond == "Minimo") {
+        foreach ($obj as $value) {
+            if ($value->nomeColonne == "Minimo") {
+                $return[$y] = $value;
+                $y++;
+            } else {
+                $i++;
+            }
+        }
+        return $return;
+    } elseif ($cond == "Contingenza") {
+        foreach ($obj as $value) {
+            if ($value->nomeColonne == "Contingenza") {
+                $return[$y] = $value;
+                $y++;
+            } else {
+                $i++;
+            }
+        }
+        return $return;
+    } elseif ($cond == "Altre Indennità") {
+        foreach ($obj as $value) {
+            if ($value->nomeColonne == "Altre Indennità") {
+                $return[$y] = $value;
+                $y++;
+            } else {
+                $i++;
+            }
+        }
+        return $return;
+    } elseif ($cond == "Importo") {
+        foreach ($obj as $value) {
+            if ($value->nomeColonne == "Importo") {
+                $return[$y] = $value;
+                $y++;
+            } else {
+                $i++;
+            }
+        }
+        return $return;
+    }
+}
+
+
 
 ?>
 
@@ -50,10 +108,10 @@ if (isset($_GET["button2"])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <style type="text/css">
-    body {
-        font: 14px sans-serif;
-        text-align: center;
-    }
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+        }
     </style>
 </head>
 
@@ -71,40 +129,96 @@ if (isset($_GET["button2"])) {
         <br><br>
 
         <div class="mx-auto" style="width: 500px">
-            <textarea class=" form-control" row="3"><?php echo $ps; ?></textarea>
+            <textarea class=" form-control"
+                row="3"><?php echo $ps; ?></textarea>
         </div>
-        <br><br>
-
-        <?php
-        if (isset($_GET["button1"])) {
-            $sql = "SELECT partiStipulanti FROM stipule";
-
-            if ($stmt = $mysqli->prepare($sql)) {
-                // Bind variables to the prepared statement as parameters
-                /*$stmt->bind_param("i", $param_ID);
-
-                // Set parameters
-                $param_ID = 1;*/
-        
-                // Attempt to execute the prepared statement
-                if ($stmt->execute()) {
-                    // Store result
-                    $stmt->store_result();
-            
-                    // Check if username exists, if yes then verify password
-                    $stmt->bind_result($ps);
-
-                    $stmt->fetch();
-                }
-            }
-        }
-?>
-
         <br><br>
         <div class="btn-group">
             <input type="submit" class="btn btn-danger" value="Cancella" name="clear">
             <a href="../index.php" class="btn btn-info">Home</a>
         </div>
+        <br><br>
+
+        <?php
+        if (isset($_GET["button1"])) {
+            $sql = "SELECT id, livello, nomeColonne, importo FROM retribuzioni";
+            $obj = $table = null;
+            $i = 0;
+            if ($stmt = $mysqli->query($sql)) {
+                while ($obj = $stmt->fetch_object()) {
+                    $table[$i] = $obj;
+                    $i++;
+                }
+                $q = tableChecker($table, "Q");
+                $minimo = tableChecker($table, "Minimo");
+                $contingenza = tableChecker($table, "Contingenza");
+                $ai = tableChecker($table, "Altre Indennità");
+                $imp = tableChecker($table, "Importo"); ?>
+        <div class="mx-auto" style="width: 30%">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Livello</th>
+                        <th scope="col">Minimo</th>
+                        <th scope="col">Contingenza</th>
+                        <th scope="col">Altre Indennità</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Q</th>
+                        <td><?php foreach ($q as $value) {
+                    if ($value->nomeColonne == "Minimo") {
+                        echo $value->importo;
+                    }
+                } ?>
+                        </td>
+                        <td><?php foreach ($q as $value) {
+                    if ($value->nomeColonne == "Contingenza") {
+                        echo $value->importo;
+                    }
+                } ?>
+                        </td>
+                        <td><?php foreach ($q as $value) {
+                    if ($value->nomeColonne == "Altre Indennità") {
+                        echo $value->importo;
+                    }
+                } ?>
+                        </td>
+                    </tr>
+                    <?php for ($i = 1; $i < 8; $i++) {
+                    ?>
+                    <tr>
+                        <th scope="row"><?php echo $i?>
+                        </th>
+                        <td><?php foreach ($minimo as $value) {
+                        if ($value->livello == $i) {
+                            echo $value->importo;
+                        }
+                    }
+                    echo "0"; ?>
+                        </td>
+                        <td><?php foreach ($contingenza as $value) {
+                        if ($value->livello == $i) {
+                            echo $value->importo;
+                        }
+                    } ?>
+                        </td>
+                        <td><?php foreach ($ai as $value) {
+                        if ($value->livello == $i) {
+                            echo $value->importo;
+                            exit;
+                        }
+                    }
+                    echo "0"; ?>
+                        </td>
+                    </tr>
+                    <?php
+                } ?>
+        </div>
+        <?php
+            }
+        }?>
 
 </body>
 
