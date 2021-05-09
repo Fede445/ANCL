e4560cd1f21d316956fe1efd02e8376ff859b0ef
TomZanna <18452@studenti.marconiverona.edu.it>
2021-05-09 21:55:47 +0200

    Add "Nessun contratto per questo settore"

--- web/to-table.php
+++ web/to-table.php
@@ -20,10 +20,33 @@ if (isset($_GET["sectorid"])) {
         ]
     );
 
-    // TODO: implementare settore non trovato/non ha documenti 
-    $table_id = $document["_id"];
-    header("location: tabella.php?id=$table_id");
+    if ($document) {
+        $table_id = $document["_id"];
+        header("location: tabella.php?id=$table_id");
+    } else {
+?>
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+            <title>Tabella | ANCL Verona</title>
+            <link rel="stylesheet" href="css/main.css">
+            <link rel="stylesheet" href="css/tabella.css">
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;500&display=swap" rel="stylesheet" />
+        </head>
+
+        <body>
+            <?php include "header.php" ?>
+
+            <main class="container">
+                <h1> ANCL UP VERONA </h1>
+                <h3>Nessun contratto per il seguente settore</h3>
+            </main>
+        </body>
+<? }
 } else {
     // Se non ci sono parametri GET ritorno ai settori
     header("location: index.php");
 }
+?>
